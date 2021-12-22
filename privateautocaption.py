ad18e91f827772e737f34cefc900674ac85b0719
Rafeeq <94901743+Rafeeq-99@users.noreply.github.com>
2021-12-22 15:12:24 +0530

    Update privateautocaption.py

--- privateautocaption.py
+++ privateautocaption.py
@@ -10,7 +10,7 @@ if __name__ == "__main__" :
     plugins = dict(
         root="mt_privateautocaption"
     )
-    MrktTech = pyrogram.Client(
+    MRKTTECH = pyrogram.Client(
         "CaptionBot",
         bot_token=Config.MRKT_BOT_TOKEN,
         api_id=Config.API_ID,
@@ -18,4 +18,4 @@ if __name__ == "__main__" :
         plugins=plugins,
         workers=300
     )
-    MrktTech.run()
+    MRKTTECH.run()
